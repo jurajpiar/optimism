7fcf36f621508bbf0466a948f8ced3cb13f3a0a4
Juraj Piar <juraj.piar@icloud.com>
2026-01-07 10:09:23 +0000

    ignore block and tx hash mismatch in receipt fetch validation

--- op-service/sources/receipts.go
+++ op-service/sources/receipts.go
@@ -26,7 +26,8 @@ func validateReceipts(block eth.BlockID, receiptHash common.Hash, txHashes []com
 	}
 	if len(txHashes) == 0 {
 		if receiptHash != types.EmptyRootHash {
-			return fmt.Errorf("no transactions, but got non-empty receipt trie root: %s", receiptHash)
+			fmt.Printf("no transactions, but got non-empty receipt trie root: %s", receiptHash)
+			// return fmt.Errorf("no transactions, but got non-empty receipt trie root: %s", receiptHash)
 		}
 	}
 	// We don't trust the RPC to provide consistent cached receipt info that we use for critical rollup derivation work.
@@ -84,8 +85,11 @@ func validateReceipts(block eth.BlockID, receiptHash common.Hash, txHashes []com
 	// or returning them out-of-order. Verify the receipts against the expected receipt-hash.
 	hasher := trie.NewStackTrie(nil)
 	computed := types.DeriveSha(types.Receipts(receipts), hasher)
+	// if receiptHash != computed {
+	// 	return fmt.Errorf("failed to fetch list of receipts: expected receipt root %s but computed %s from retrieved receipts", receiptHash, computed)
+	// }
 	if receiptHash != computed {
-		return fmt.Errorf("failed to fetch list of receipts: expected receipt root %s but computed %s from retrieved receipts", receiptHash, computed)
+		fmt.Printf("failed to fetch list of receipts: expected receipt root %s but computed %s from retrieved receipts", receiptHash, computed)
 	}
 	return nil
 }
